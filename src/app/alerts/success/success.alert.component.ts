64f97e68881bbdf2b9ddecb81b6282e58f47e8cf
Meowmir <disewji@gmail.com>
2024-01-29 14:52:58 +0100

    assignment: add warning and success alert

---
+++ src/app/alerts/success/success.alert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-success-alert",
+  templateUrl: "./success.alert.component.html",
+  styleUrl: "../../app.component.css",
+})
+export class SuccessAlertComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { AlertsComponent } from "./alerts/alerts.component";
+import { WarningAlertComponent } from "./alerts/warning/warning.alert.component";
+import { SuccessAlertComponent } from "./alerts/success/success.alert.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    AlertsComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

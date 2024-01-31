e6a58efbdb0b0b877c54196f8ba3019bfcadf45e
Meowmir <disewji@gmail.com>
2024-01-31 16:47:05 +0100

    bp with header, recipies and shopping list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,25 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
+import { HeaderComponent } from "./components/header/header.component";
+import { RecipiesComponent } from "./components/recipies/recipies.component";
+import { RecipieListComponent } from "./components/recipies/recipie-list/recipie-list.component";
+import { RecipieDetailComponent } from "./components/recipies/recipie-detail/recipie-detail.component";
+import { RecipieItemComponent } from "./components/recipies/recipie-list/recipie-item/recipie-item.component";
+import { ShoppingListComponent } from "./components/shopping-list/shopping-list.component";
+import { ShoppingEditComponent } from "./components/shopping-list/shopping-edit/shopping-edit.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipieListComponent,
+    RecipieDetailComponent,
+    RecipieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],

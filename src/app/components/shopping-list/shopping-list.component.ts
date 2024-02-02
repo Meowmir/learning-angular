1ed03bb72a3cf013c077b5ed5464542fe1c7edb4
Meowmir <disewji@gmail.com>
2024-02-02 09:32:26 +0100

    add ingredients to shopping list, add buttons for add, delete, clear form of ingredients

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Ingredient } from "../../shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrl: "./shopping-list.component.css",
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient("Cucumber", 1),
+    new Ingredient("Tomatoes", 5),
+  ];
 }

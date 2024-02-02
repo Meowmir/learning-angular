67526d50802148aace94d377461ac0a1793e3520
Meowmir <disewji@gmail.com>
2024-02-02 08:42:28 +0100

    correct spelling error, add responsive navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
 import { HeaderComponent } from "./components/header/header.component";
-import { RecipiesComponent } from "./components/recipies/recipies.component";
-import { RecipieListComponent } from "./components/recipies/recipie-list/recipie-list.component";
-import { RecipieDetailComponent } from "./components/recipies/recipie-detail/recipie-detail.component";
-import { RecipieItemComponent } from "./components/recipies/recipie-list/recipie-item/recipie-item.component";
+import { RecipesComponent } from "./components/recipes/recipes.component";
+import { RecipeListComponent } from "./components/recipes/recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./components/recipes/recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./components/recipes/recipe-list/recipe-item/recipe-item.component";
 import { ShoppingListComponent } from "./components/shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./components/shopping-list/shopping-edit/shopping-edit.component";
 
@@ -15,10 +15,10 @@ import { ShoppingEditComponent } from "./components/shopping-list/shopping-edit/
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipiesComponent,
-    RecipieListComponent,
-    RecipieDetailComponent,
-    RecipieItemComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
   ],

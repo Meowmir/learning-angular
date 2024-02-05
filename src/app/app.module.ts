1b04e9e9e48fa2902798e6bcc85730f53d701132
Meowmir <disewji@gmail.com>
2024-02-05 13:32:34 +0100

    clear master branch of server components and alert components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,10 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { ServerComponent } from "./server/server.component";
-import { ServersComponent } from "./servers/servers.component";
-import { AlertsComponent } from "./alerts/alerts.component";
-import { WarningAlertComponent } from "./alerts/warning/warning.alert.component";
-import { SuccessAlertComponent } from "./alerts/success/success.alert.component";
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    AlertsComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],

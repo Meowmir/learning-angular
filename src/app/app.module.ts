9e42e75bb0a2114c956d4fb4f703a1470f4193e1
Meowmir <disewji@gmail.com>
2024-02-05 13:31:31 +0100

    add buttons for starting and stopping an event of incrementing numbers, display numbers with odd or even in front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
-import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { ServerComponent } from "./server/server.component";
-import { ServersComponent } from "./servers/servers.component";
-import { AlertsComponent } from "./alerts/alerts.component";
-import { WarningAlertComponent } from "./alerts/warning/warning.alert.component";
-import { SuccessAlertComponent } from "./alerts/success/success.alert.component";
-import { FormsModule } from "@angular/forms";
+import { GameControlComponent } from "./game-control/game-control.component";
+import { OddComponent } from "./odd/odd.component";
+import { EvenComponent } from "./even/even.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    AlertsComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],

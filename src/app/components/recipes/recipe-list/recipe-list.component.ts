4b0260fb383264d5fb4f89b3a09759bfcb955b16
Meowmir <disewji@gmail.com>
2024-02-05 15:36:15 +0100

    add event and property binding to pass data between recipe components, change test recipes

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 import { Recipe } from "../recipe";
 
 @Component({
@@ -7,16 +7,22 @@ import { Recipe } from "../recipe";
   styleUrl: "./recipe-list.component.css",
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
-      "A test recipe",
-      "This is simply a test",
+      "Christmas Raisin Scones",
+      "Holiday Raisin Scones can actually be made with any toppings (savory or sweet) in place of raisins. When baked, the scone will be rich! Holiday raisin cake is an ideal addition to the Christmas table. Find out how it is prepared!",
       "https://www.slobodenpecat.mk/wp-content/uploads/2024/01/%D0%B1%D0%BE%D0%B6%D0%B8%D1%9C%D0%BD%D0%B0-%D0%BF%D0%BE%D0%B3%D0%B0%D1%87%D0%B0-%D1%81%D0%BE-%D1%81%D1%83%D0%B2%D0%BE-%D0%B3%D1%80%D0%BE%D0%B7%D1%98%D0%B5-%D0%A4%D0%BE%D1%82%D0%BE-Rawpixel.webp",
     ),
     new Recipe(
-      "A test recipe",
-      "This is simply a test",
-      "https://www.slobodenpecat.mk/wp-content/uploads/2024/01/%D0%B1%D0%BE%D0%B6%D0%B8%D1%9C%D0%BD%D0%B0-%D0%BF%D0%BE%D0%B3%D0%B0%D1%87%D0%B0-%D1%81%D0%BE-%D1%81%D1%83%D0%B2%D0%BE-%D0%B3%D1%80%D0%BE%D0%B7%D1%98%D0%B5-%D0%A4%D0%BE%D1%82%D0%BE-Rawpixel.webp",
+      "Simple Crusty White Bread",
+      "Baking a crusty loaf of country-style white bread at home is made simple with a Dutch oven and digital scale.",
+      "https://www.seriouseats.com/thmb/LoXQL7Yp_uXxtipH8cCp_LGVg5E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2014__08__20140810-workhorse-bread-vicky-wasik-3-3a86ee51da2e4a7b8239ceb62d8d8d17.jpg",
     ),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }

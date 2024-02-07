2a5e5493f8d89f3151f6e8bce6401af236bd83fe
Meowmir <disewji@gmail.com>
2024-02-07 15:06:03 +0100

    add ingredients to shopping list via button in recipe details

--- src/app/components/recipes/recipe.service.ts
+++ src/app/components/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import {RecipeModel} from "./recipe.model";
-import {EventEmitter} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
 import {Ingredient} from "../../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<RecipeModel>()
 
@@ -11,8 +13,8 @@ export class RecipeService {
       "Holiday Raisin Scones can actually be made with any toppings (savory or sweet) in place of raisins. When baked, the scone will be rich! Holiday raisin cake is an ideal addition to the Christmas table. Find out how it is prepared!",
       "https://www.slobodenpecat.mk/wp-content/uploads/2024/01/%D0%B1%D0%BE%D0%B6%D0%B8%D1%9C%D0%BD%D0%B0-%D0%BF%D0%BE%D0%B3%D0%B0%D1%87%D0%B0-%D1%81%D0%BE-%D1%81%D1%83%D0%B2%D0%BE-%D0%B3%D1%80%D0%BE%D0%B7%D1%98%D0%B5-%D0%A4%D0%BE%D1%82%D0%BE-Rawpixel.webp",
       [
-        new Ingredient('Flour', 380),
-        new Ingredient('Sugar', 80)
+        new Ingredient('Flour', 38),
+        new Ingredient('Sugar', 8)
       ]
       ),
     new RecipeModel(
@@ -20,13 +22,20 @@ export class RecipeService {
       "Baking a crusty loaf of country-style white bread at home is made simple with a Dutch oven and digital scale.",
       "https://www.seriouseats.com/thmb/LoXQL7Yp_uXxtipH8cCp_LGVg5E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2014__08__20140810-workhorse-bread-vicky-wasik-3-3a86ee51da2e4a7b8239ceb62d8d8d17.jpg",
       [
-        new Ingredient('Flour', 1000),
-        new Ingredient('Water', 700)
+        new Ingredient('Flour', 1),
+        new Ingredient('Water', 7)
       ]
     ),
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
   getRecipes() {
     return this.recipes.slice()
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.shoppingListService.addIngredients(ingredients)
   }
 }

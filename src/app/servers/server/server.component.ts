44256df84133f65cb2bb4bca9d4b911c8f7b04cc
Meowmir <disewji@gmail.com>
2024-02-15 15:50:09 +0100

    add resolve guard

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute, Params, Router} from "@angular/router";
+import {ActivatedRoute, Data, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-server',
@@ -16,6 +16,13 @@ export class ServerComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
+    this.route.data
+      .subscribe(
+        (data: Data) => {
+          this.server = data['server']
+        }
+      )
+    /*
     const id = +this.route.snapshot.params['id']
     this.server = this.serversService.getServer(1);
     this.route.params
@@ -24,6 +31,7 @@ export class ServerComponent implements OnInit {
           this.server = this.serversService.getServer(+params['id'])
         }
       )
+     */
   }
 
   onEdit(){

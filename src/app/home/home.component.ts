47c36b8b567521a89af065e3c25f44af96adedc8
Meowmir <disewji@gmail.com>
2024-02-26 15:18:44 +0100

    handling errors

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,6 +20,9 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count > 3) {
+          observer.error(new Error("Count is greater than 3!!"));
+        }
         count++;
       }, 1000);
     });

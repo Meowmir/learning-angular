010d6884ca2429779ab37452e1d4cb420409aab5
Meowmir <disewji@gmail.com>
2024-02-27 11:33:27 +0100

    adding default username and question

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from "@angular/forms";
 })
 export class AppComponent {
   @ViewChild("f") signupForm: NgForm;
+  defaultQuestion = "pet";
+  defaultUsername = "User" + Math.random();
 
   suggestUserName() {
     const suggestedName = "Superuser";

772781646d29068d20dca3d35daa90655ade1bb8
Meowmir <disewji@gmail.com>
2024-02-27 14:56:30 +0100

    use collected data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,14 @@ export class AppComponent {
   defaultUsername = "User" + Math.random();
   answer = "";
   genders = ["male", "female"];
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: "",
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = "Superuser";
@@ -40,6 +48,11 @@ export class AppComponent {
    */
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.userData.gender;
   }
 }

fbca4cf5eea79abb50ecdc7fa06246767809616a
Meowmir <disewji@gmail.com>
2024-02-28 11:39:35 +0100

    add input fields, display data entered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
+  @ViewChild("f") signupForm: NgForm;
+  defaultSub = "advanced";
+  user = {
+    email: "",
+    subscription: "",
+    password: "",
+  };
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.subscription = this.signupForm.value.userData.subscription;
+    this.user.password = this.signupForm.value.userData.password;
+  }
 }
